28a22e3cb0de72ffa0c8e1c68486ed2f0eebcfa3
dossy007 <toshi@toshinoMacBook-ea.local>
2019-03-02 15:30:44 +0900

    Add routes pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "pages#index"
   resources :pages
+  get 'pages',to: 'pages#uno'
+  get 'pages',to: 'pages#due'
+  get 'pages',to: 'pages#tre'
+  get 'pages',to: 'pages#quattro'
 end
