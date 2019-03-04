6be8c2fb483b466c4c55d526af9ce970d15dd4d9
dossy007 <toshi@toshinoMacBook-ea.local>
2019-03-04 20:42:38 +0900

    Edit routes page nest
    Nestさせた

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "pages#index"
-  resources :pages
-  get 'pages',to: 'pages#uno'
-  get 'pages',to: 'pages#due'
-  get 'pages',to: 'pages#tre'
-  get 'pages',to: 'pages#quattro'
+  resources :pages do
+  	member do
+    # get 'pages',to: 'pages#uno'
+    get 'uno'
+    get 'due'
+    get 'tre'
+    get 'quattro'
+    end
+  end
+  # resources :photos do
+  #   member do
+  #     get 'preview'
+  #   end
+  # end
+  # get 'pages',to: 'pages#due'
 end
