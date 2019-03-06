b3a66b5ffc21a1ab77a526a71e315cd3ec009eba
dossy007 <toshi@toshinoMacBook-ea.local>
2019-03-06 18:03:19 +0900

    Add create action private

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,14 @@ class PagesController < ApplicationController
 	def index
 	end
 
-	def create
-	end
+	# def create
+	# 	@page = Page.create(:text => create_params[:text],:name => params[:name],:image => "")
+	# 	if @choco.valid?
+	# 		redirect_to choco_path(@choco.id)
+	#     else
+	#     	redirect_to new_choco_path
+	#     end
+	# end
 
 	def show
 		@pages = Page.find(params[:id])
@@ -19,5 +25,20 @@ class PagesController < ApplicationController
 	end
 
 	def quattro
+	end
+
+	def change
+		@page = ""
+		binding.pry
+	end
+
+	private
+	def create_params #textを保存
+		params.permit(:text)
+	end
+
+	def create_values ##valueのみを取得する処理 "ダイヤモンド"
+		params[:text]
+		# params[:text] || ""
 	end
 end
