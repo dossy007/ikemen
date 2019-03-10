d306cbf02e31eb74387026c3b0bff9a19d546136
dossy007 <toshi@toshinoMacBook-ea.local>
2019-03-10 17:57:52 +0900

    Add initialize

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,6 +30,8 @@ Rails.application.configure do
   config.assets.compile = true
   config.assets.initialize_on_precompile = false
 
+  config.assets.initialize = false
+
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
